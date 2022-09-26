bff20fd599c2c5e4baccc7fca75c5b7250ddcffd
Hongbusi <coderhbs@gmail.com>
2022-09-26 18:20:49 +0800

    chore: fetch commit

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,11 +1,28 @@
 import { Octokit } from 'octokit'
+import ora from 'ora'
+import chalk from 'chalk'
+import consola from 'consola'
+
+interface Repository {
+  name: string
+  defaultBranchRef: {
+    name: string
+  }
+}
+
+interface GraphQLResponse {
+  search: {
+    repositoryCount: number
+    edges: Record<'node', Repository>[]
+  }
+}
 
 const octokit = new Octokit({
   // auth: process.env.GITHUB_TOKEN
-  auth: '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'
+  auth: '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'
 })
 
-const fetchRepositories = async (name: string) => {
+const searchRepositories = async (name: string) => {
   const query = `{
     search(query: "user:${name}", type: REPOSITORY, first: 100) {
       repositoryCount
@@ -13,18 +30,75 @@ const fetchRepositories = async (name: string) => {
         node {
           ... on Repository {
             name
+            defaultBranchRef {
+              name
+            }
           }
         }
       }
     }
   }`
 
-  const response = (await octokit.graphql(query)).search
-  return response
+  // eslint-disable-next-line prefer-const
+  let { repositoryCount, edges } = (await octokit.graphql<GraphQLResponse>(query)).search
+
+  const repositories = edges.filter((i) => {
+    if (!i.node.defaultBranchRef) {
+      repositoryCount--
+      return false
+    }
+    return true
+  }).map(i => ({ name: i.node.name, defaultBranch: i.node.defaultBranchRef.name }))
+
+  return {
+    repositoryCount,
+    repositories
+  }
+}
+
+const fetchCommits = async (owner: string, name: string, branch: string) => {
+  const query = `{
+    repository(owner: "${owner}", name: "${name}") {
+      object(expression: "${branch}") {
+        ... on Commit {
+          history {
+            nodes {
+              oid
+              author {
+                avatarUrl
+                email
+                name
+              }
+            }
+            pageInfo {
+              hasNextPage
+              endCursor
+            }
+          }
+        }
+      }
+    }
+  }`
+
+  const response = (await octokit.graphql(query)).repository.object
 }
 
 export async function getOrganizationContributors(name: string) {
+  const spinner = ora(chalk.green(`Searching for top 100 repositories in ${name}...`)).start()
 
+  const { repositoryCount, repositories } = await searchRepositories(name)
+
+  spinner.succeed(chalk.green(`${repositoryCount} repositories found.`))
+
+  spinner.start('Fetching contributors...')
+
+  do {
+    const repository = repositories.shift()
+    spinner.text = `Fetching ${repository.name} contributors... (${repositoryCount - repositories.length}/${repositoryCount})`
+    await fetchCommits(name, repository.name, repository.defaultBranch)
+  } while (repositories.length > 0)
+
+  spinner.succeed(chalk.green('Fetched contributors success.'))
 }
 
-fetchRepositories('developer-plus')
+getOrganizationContributors('developer-plus')
